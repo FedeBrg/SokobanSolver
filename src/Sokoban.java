2b4dea250b21ddbd62665a3b7f56bdca3b21fa9f
FedeBrg <fede.bergagna@gmail.com>
2020-03-16 20:10:42 -0300

    Move player and Push box

--- src/Sokoban.java
+++ src/Sokoban.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Sokoban {
 
     int cols;
@@ -6,17 +8,77 @@ public class Sokoban {
 
     }
 
-    String movePlayer(int x, int y, int dx, int dy, String board){
+    Board movePlayer(Board b, int dx, int dy){
+        int x = b.getPlayerx();
+        int y = b.getPlayery();
+
+
         int newPos = x+dx+(y+dy)*cols;
-        if(board.charAt(newPos) == '#' || board.charAt(newPos) == '@'){
+        int playerPos = x+y*cols;
+        char replacement;
+        char leftBehind;
+
+        char c = b.getBoard().charAt(newPos);
+        if(c == '#' || c == '$'){
             return null;
         }
+        else if(c == '.'){
+            replacement = 'O';
+        }
+        else{
+            replacement = '@';
+        }
 
-        return null;
+        c = b.getBoard().charAt(playerPos);
+        if(c == 'O'){
+            leftBehind = '.';
+        }
+        else{
+            leftBehind = ' ';
+        }
+
+
+        char[] boardArray = b.getBoard().toCharArray();
+        boardArray[playerPos] = leftBehind;
+        boardArray[newPos] = replacement;
+
+        return new Board(new String(boardArray),b.getSolution().concat(new String((boardArray))),x+dx,y+dy);
     }
 
-    String pushBox(int x, int y, int dx, int dy, String board){
-        return null;
+    Board pushBox(Board b, int dx, int dy){
+        int x = b.getPlayerx();
+        int y = b.getPlayery();
+        int newPos = x+(2*dx) + (y+(2*dy))*cols;
+        int boxPos = x+dx+(y+dy)*cols;
+
+        char replacement;
+        char leftBehind;
+
+        char c = b.getBoard().charAt(newPos);
+        if(c == '#' || c == '$' || c == '%'){
+            return null;
+        }
+        else if(c == '.'){
+            replacement = '%';
+        }
+        else{
+            replacement = '$';
+        }
+
+        c = b.getBoard().charAt(boxPos);
+        if(c == '%'){
+            leftBehind = '.';
+        }
+        else{
+            leftBehind = ' ';
+        }
+
+        char[] boardArray = b.getBoard().toCharArray();
+        boardArray[x+y*cols] = leftBehind;
+        boardArray[newPos] = replacement;
+
+
+        return movePlayer(new Board(new String(boardArray),b.getSolution(),x,y),dx,dy);
     }
 
     boolean isSolution(String board){
