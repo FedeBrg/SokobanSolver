90ff2083a7a9456d5c99b03c0c2f6810f7478996
Manuel <manuel.rbrizi@gmail.com>
2020-03-29 19:31:00 -0300

    IDA* search working correctly

--- src/implementations/IDAStar.java
+++ src/implementations/IDAStar.java
@@ -7,7 +7,6 @@ import interfaces.Sokoban;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Stack;
 
 public class IDAStar implements SearchMethod {
 
@@ -22,49 +21,76 @@ public class IDAStar implements SearchMethod {
         Board currentBoard;
         Board resultBoard;
         boardQueue.add(b);
+        currentBoard = boardQueue.peek();
+        limit = currentBoard.getCost() + currentBoard.getHeuristic();
 
-        while(true){
-            currentBoard = boardQueue.poll();
-            if (currentBoard == null) {
-                return null;
+        /* Mientras tengamos chance de encontrar la solucion */
+        while(!boardQueue.isEmpty()){
+
+            /* Hasta que encontremos un estado con f mayor... */
+            while(currentBoard.getHeuristic() + currentBoard.getCost() <= limit){
+                //System.out.printf("LIMIT = %d\n", limit);
+
+                /* Buscamos la solución */
+                resultBoard = findPathIDAStar(currentBoard, limit, directions, s);
+
+                /* La solucion es esta */
+                if(resultBoard != null){
+                    return resultBoard;
+                }
+
+                currentBoard = boardQueue.poll();
+
+                /* No mas estados = No solucion */
+                if(currentBoard == null){
+                    return null;
+                }
             }
 
+            /* El nuevo limite esta en el nodo que rompio el while de arriba */
             limit = currentBoard.getCost() + currentBoard.getHeuristic();
-            resultBoard = specialIDDFS(currentBoard, limit, directions, s);
-
-            if(resultBoard != null){
-                return resultBoard;
-            }
         }
+
+        /* Si llegamos hasta aca algo anda mal */
+        return null;
     }
 
-    private Board specialIDDFS(Board b, int limit, int[][] directions, Sokoban s){
+    private Board findPathIDAStar(Board b, int limit, int[][] directions, Sokoban s){
         Board resultBoard;
         Board nextStep = null;
         int totalCost = b.getCost() + b.getHeuristic();
 
+        /* Si es solución llevamos devolvemos el nodo, q tiene todo el camino */
         if(Utilities.isSolution(b)){
             return b;
         }
 
+        /* Si es mayor del limite vuelvo para atras pero con null porque todavia no es solucion
+        *  Si no esta en la cola, lo agrego para que sea el nuevo limite
+        */
         else if(totalCost > limit){
+            if(!specialVisited.containsKey(b.getBoard())){
+                boardQueue.add(b);
+            }
             return null;
         }
 
         else{
-            specialVisited.put(b.getBoard(), limit);
+            /* Lo pongo como visitado sino expande todos los nodos indiscriminadamente y para eso
+             * necesito el heap del tamaño de una casa
+             */
+            specialVisited.put(b.getBoard(), totalCost);
 
+            /* Vemos que camino es valido... */
             for (int[] direction : directions) {
                 if ((resultBoard = s.move(b, direction[0], direction[1])) != null) {
                     resultBoard.setCost(b.getCost()+1);
                     resultBoard.setHeuristic(s.getHeuristic(resultBoard));
 
-                    if(!specialVisited.containsKey(resultBoard.getBoard())){
-                        boardQueue.add(resultBoard);
-                    }
-
-                    nextStep = specialIDDFS(resultBoard, limit,directions, s);
+                    /* Desde el nuevo nodo, nos tiramos a buscar el resto del camino */
+                    nextStep = findPathIDAStar(resultBoard, limit,directions, s);
                     if(nextStep != null){
+                        /* Encontre la solucion, la sigo llevando */
                         return nextStep;
                     }
                 }
